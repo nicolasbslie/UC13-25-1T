4ba3ad923b35be60f45457c948918efb08f0511f
Nicolas <nicolasbsliell@gmail.com>
2026-06-11 16:14:39 -0300

    feat: criando o server e database da biblioteca

---
+++ 11-06-Biblioteca/src/server.ts
@@ -0,0 +1,68 @@
+import express  from "express";
+import { pool } from "./database";
+
+const app = express()
+const PORT = 3000
+app.use(express.json())
+
+//Livros precisam de Título, Autor, Editora e Páginas
+
+//Listar livros
+app.get("/livros", async (req, res) => {
+    try {
+        const [titulos] = await pool.query("SELECT * FROM titulos")
+        return res.status(200).json(titulos)
+    } catch(erro) {
+        console.log("Erro: ", erro)
+        return res.status(500).json("Erro ao buscar os títulos dos livros: " + erro)
+    }
+})
+
+//CRIA livros
+app.post("/livros", async (req, res) => {
+    try {
+        const { nome_livro, autor, editora, paginas } = req.body
+
+        const [resultado] = await pool.query(
+            "INSERT INTO titulos (nome_livro, autor, editora, pagina) VALUES (?, ?, ?, ?)",
+            [nome_livro, autor, editora, paginas]
+        )
+        return res.status(201).json("Livro cadastrado com sucesso!")
+    } catch(erro) {
+        return res.status(500).json("Erro interno do servidor: " + erro)
+    }
+})
+
+//UPDATE
+app.put("/livros/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const { nome_livro, autor, editora, paginas } = req.body;
+
+    const [resultado] = await pool.query(
+      "UPDATE titulos SET nome_livro = ?, autor = ?, editora = ?, paginas = ? WHERE id = ?",
+      [nome_livro, autor, editora, paginas, id]
+    );
+
+    return res.status(200).json("Livro atualizado com sucesso!");
+  } catch (erro) {
+    return res.status(500).json("Erro interno do servidor: " + erro);
+  }
+});
+
+//DELETE
+app.delete("/livros/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const [resultado] = await pool.query(
+      "DELETE FROM titulos WHERE id = ?",
+      [id]
+    );
+
+    return res.status(200).json("Livro deletado");
+  } catch (erro) {
+    return res.status(500).json("Erro interno do servidor: " + erro);
+  }
+});
